0adebde20dc1c8ae774f14bef376fb417b3e3ce7
Lucas Silva <jacks2kar@gmail.com>
2020-01-16 11:18:26 -0300

    Inicio do Backend do RadarDev

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const axios = require("axios");
+const routes = Router();
+const Dev = require("./models/Dev");
+
+routes.post("/devs", async (request, response) => {
+  const { github_username, techs } = request.body;
+
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = apiResponse.data;
+
+  const techsArray = techs.split(",").map(tech => tech.trim());
+
+  await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  return response.json({ message: "Hello Omnistack" });
+});
+
+module.exports = routes;
