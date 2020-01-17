b770a3970512f5c744e4f8a6f7525fb7af07a292
Lucas Silva <jacks2kar@gmail.com>
2020-01-17 11:12:07 -0300

    finalização cadastro e busca de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,12 @@
 const { Router } = require("express");
-const axios = require("axios");
+const DevController = require("./Controllers/DevController");
+const SearchController = require("./Controllers/SearchController");
+
 const routes = Router();
-const Dev = require("./models/Dev");
 
-routes.post("/devs", async (request, response) => {
-  const { github_username, techs } = request.body;
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
 
-  const apiResponse = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(",").map(tech => tech.trim());
-
-  await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray
-  });
-
-  return response.json({ message: "Hello Omnistack" });
-});
+routes.get("/search", SearchController.index);
 
 module.exports = routes;
